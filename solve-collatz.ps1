0d6c4fbdbc9f9a514835ac3418b5bfc215b23b32
Dennis <52612181+sudo-SHA512@users.noreply.github.com>
2023-01-27 09:11:13 +0100

    Create solve-collatz.ps1

---
+++ solve-collatz.ps1
@@ -0,0 +1,30 @@
+function Do-Collatz{
+    param(
+         [Parameter(Mandatory)]
+         [long]$Number
+     )
+    $count = 0;
+    $maximum = 0;
+    $start = $Number
+    $Results = @()
+
+    while($Number -gt 1){
+    if($Number %2 -gt 0){
+    $Number = ($Number*3)+1
+    }else{
+    $Number = $Number/2
+    }
+    $count++;
+    if($Number -gt $maximum){
+    $maximum = $Number
+    }
+    }
+     
+    $Results += [PSCustomObject] @{
+    Start = $start
+    Count = $count
+    Maximum = $maximum
+    }
+
+    return $Results
+}
